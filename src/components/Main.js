0f30644caef6c52134770970e9211dd7836ed9db
matiaswasiak <matiaswasiak@gmail.com>
2022-06-25 00:02:59 -0300

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,34 @@ import styled from "styled-components";
 const Main = (props) => {
   return (
     <Container>
-      <ShareBox>Share</ShareBox>
+      <ShareBox>
+        Share
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+
+          <button>
+            <img src="/images/video-icon.svg" alt="" />
+            <span>Video</span>
+          </button>
+
+          <button>
+            <img src="/images/event-icon.svg" alt="" />
+            <span>Event</span>
+          </button>
+
+          <button>
+            <img src="/images/article-icon.svg" alt="" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
     </Container>
   );
 };
@@ -21,6 +48,21 @@ const CommonCard = styled.div`
   position: relative;
   border: none;
   box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 25%);
+
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+  }
 `;
 
 const ShareBox = styled(CommonCard)`
